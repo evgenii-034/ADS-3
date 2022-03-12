dc221d8aaf5421207da0b6979b61183b6db9c7b4
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-03-12 13:07:17 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -2,11 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
 return 0; // если ничего не найдено
-int x = 0;
+int k = 0;
 for (int i = 0; i < size; i++) {
 if (arr[i] == value) {
-x += 1;
+k += 1;
 }
 }
-return x;
+return k;
 }
