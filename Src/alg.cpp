3a789d69b80a81b25d70b285f9b00217489a3bca
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-03-12 13:35:42 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

---
+++ Src/alg.cpp
@@ -0,0 +1,26 @@
+// Copyright 2021 NNTU-CS
+
+int cbinsearch(int *arr, int size, int value) {
+int h = size - 1;
+int l = 0;
+int mid;
+while (l <= size) {
+while (l <= h) {
+mid = (l + h) / 2;
+if (value == *(arr + mid)) {
+l = h = mid;
+while (*(arr + h) == *(arr + h + 1)) {
+h++;
+}
+while (*(arr + l) == *(arr + l - 1)) {
+l--;
+}
+return h - l + 1;
+} else if (value > *(arr + mid)) {
+l = mid + 1;
+} else if (value < *(arr + mid)) {
+h = mid - 1;
+}
+}
+return 0;
+}
